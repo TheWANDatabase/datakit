9d8ba46638234f79a24b6b320d4594981e6d0be5
Altrius <tom.b.2k2@gmail.com>
2023-11-07 21:23:41 +0000

    expanding cdn metrics for more advanced graphing

--- src/schema/management/cdn.ts
+++ src/schema/management/cdn.ts
@@ -8,14 +8,23 @@ export const cdn = managementSchema.table('cdn', {
   entitySizeTotal: integer('entity_size_total').notNull(),
   entitySizeDelta: integer('entity_size_delta').notNull(),
   entitySizeAverage: numeric('entity_size_average').notNull(),
+  entitySizeMin: integer('entity_size_min').notNull(),
+  entitySizeMax: integer('entity_size_max').notNull(),
+  entitySizeDeviation: numeric('entity_size_deviation').notNull(),
 
   entityCountTotal: integer('entity_count_total').notNull(),
   entityCountDelta: integer('entity_count_delta').notNull(),
   entityCountAverage: numeric('entity_count_average').notNull(),
+  entityCountMin: integer('entity_count_min').notNull(),
+  entityCountMax: integer('entity_count_max').notNull(),
+  entityCountDeviation: numeric('entity_count_deviation').notNull(),
 
   queueBacklogTotal: integer('queue_backlog_total').notNull(),
   queueBacklogDelta: integer('queue_backlog_delta').notNull(),
   queueBacklogAverage: numeric('queue_backlog_average').notNull(),
+  queueBacklogMin: integer('queue_backlog_min').notNull(),
+  queueBacklogMax: integer('queue_backlog_max').notNull(),
+  queueBacklogDeviation: numeric('queue_backlog_deviation').notNull(),
 
   entityCountsByType: jsonb('entity_counts_by_type').notNull().default({}),
 
