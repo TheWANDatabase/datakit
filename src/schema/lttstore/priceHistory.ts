553860f4db3f006079119d3a2720bebdcf6ef65a
Arthur <arthur@thewandb.com>
2023-12-23 01:17:39 +0000

    bump datakit version and see if it fixes issue causing failures
    Signed-off-by: Arthur <arthur@thewandb.com>

---
+++ src/schema/lttstore/priceHistory.ts
@@ -0,0 +1,5 @@
+import {dataSchema} from "../schema";
+
+// TODO: Add price history tables for products, and also variants
+// Include product/variant ID, stock status, sale status, price, sale price, time (hour aligned),
+export const productPriceHistory = dataSchema.table('', {})
