6273f1806381423bf6d3b05c426777079b328f46
Arthur <arthur@thewandb.com>
2023-12-25 04:05:16 +0000

    bump datakit
    Signed-off-by: Arthur <arthur@thewandb.com>

--- src/schema/users/settings.ts
+++ src/schema/users/settings.ts
@@ -1,9 +1,14 @@
-import {text, uuid} from "drizzle-orm/pg-core";
+import {boolean, index, text, uuid} from "drizzle-orm/pg-core";
 import {dataSchema} from "../schema";
 
 export const userSettings = dataSchema.table('users_settings', {
   uid: uuid('uuid').primaryKey().unique().defaultRandom(),
-  username: text('username'),
-  email: text('email'),
-  password: text('password')
+  username: text('username').unique().notNull(),
+  email: text('email').unique().notNull(),
+  password: text('password'),
+  canLogIn: boolean('can_log_in').default(true)
+}, (table) => {
+  return {
+    emailIdx: index('email_idx').on(table.email)
+  }
 })
